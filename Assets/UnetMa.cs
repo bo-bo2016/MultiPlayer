1e7f16379d1537c97e2445078e7c3420eacc0c33
严玉波 <yyb0014@chijiangvr.com>
2017-09-06 16:55:43 +0800

    feat: add project

---
+++ Assets/UnetMa.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class UnetMa : NetworkManager {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        var player = Instantiate(playerPrefab);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+    // Update is called once per frame
+    void Update () {
+
+		
+	}
+}
